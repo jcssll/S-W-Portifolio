45ff58aece38d2830dbd00cbc6100f044cf8df61
jcssll <essentialnetworker@gmail.com>
2022-05-24 17:30:26 -0400

    Update drawer.js
    Created a toggle for the drawer to close

--- nextjs_landing_page/src/components/drawer.js
+++ nextjs_landing_page/src/components/drawer.js
@@ -29,8 +29,16 @@ export default function Drawer ({
       duration={'0.4s'}
       {...props}
       >
-
+        {closeButton && (
+          <Box as = "div" onClick={toggleHandler} sx={closeBtnStyle}>
+            {closeButton}
+          </Box>
+        )}
+        <Box sx={drawerStyle}>{children}</Box>
       </RcDrawer>
+      <Box className="drawer__handler" style={{display: 'inline-block'}} onClick={toggleHandler}>
+        {drawerHandler}
+      </Box>
     </Fragment>
   );
 };
